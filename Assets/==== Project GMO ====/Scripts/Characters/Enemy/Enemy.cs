2ab3407308dae6950f3af8aa94073dfed8063936
Yik Fung Tan <50047634+yikfungtan99@users.noreply.github.com>
2021-10-25 22:53:06 +0800

    Game Director Simple Wave Spawning

--- Assets/==== Project GMO ====/Scripts/Characters/Enemy/Enemy.cs
+++ Assets/==== Project GMO ====/Scripts/Characters/Enemy/Enemy.cs
@@ -1,11 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public class DeathEventArgs : EventArgs
+{
+    public bool isLastEnemy = false;
+}
 
 public class Enemy : Character, ICanBeDamage
 {
 
     [SerializeField] private ItemObject drops;
+
+    public event EventHandler OnDeath;
 
     public override void ReceiveDamage(Damage damage)
     {
@@ -19,7 +27,13 @@ public class Enemy : Character, ICanBeDamage
         if(Health <= 0)
         {
             GetComponent<Dispenser>().Dispense(drops);
+            DeathEvent(new DeathEventArgs {});
             Destroy(gameObject);
         }
+    }
+
+    private void DeathEvent(DeathEventArgs e)
+    {
+        OnDeath?.Invoke(this, e);
     }
 }
--- Assets/GameDirector.cs
+++ Assets/GameDirector.cs
@@ -16,27 +16,83 @@ public class GameDirector : MonoBehaviour
 
     [SerializeField] private GameObject enemy;
 
+    [SerializeField] private float spawnPace;
+    [SerializeField] private int spawnPack;
+    private float spawnTime;
+
+    [SerializeField] private int waveCount;
+    [SerializeField] private int spawnCount;
+    private int currentSpawn = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentSpawn = spawnCount;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.K))
+        SpawnWave();
+    }
+
+    private void SpawnWave()
+    {
+        bool enemyAwaitSpawn = currentSpawn > 0 && spawnTime > 0;
+
+        if (enemyAwaitSpawn)
+        {
+            spawnTime -= Time.deltaTime;
+        }
+        else
         {
             SpawnEnemy();
+            spawnTime = spawnPace;
         }
     }
 
     public void SpawnEnemy()
     {
-        GameObject enemyInstance = Instantiate(enemy);
+        int maxPackCount = currentSpawn > spawnPack ? spawnPack : currentSpawn;
+        int minPackCount = currentSpawn <= 0 ? 0 : 1;
 
-        enemyInstance.GetComponent<EnemyTarget>().SetObjective(objectiveTarget);
+        Debug.Log(minPackCount);
+        Debug.Log(maxPackCount);
 
-        spawnLocations[Random.Range(0, spawnLocations.Count)].Spawn(enemyInstance);
+        int randomPackCount = Random.Range(minPackCount, maxPackCount);
+
+        bool lastPack = currentSpawn - randomPackCount == 0;
+
+        for (int i = 0; i < randomPackCount; i++)
+        {
+            bool lastEnemy = i == randomPackCount - 1;
+
+            GameObject enemyInstance = Instantiate(enemy);
+            enemyInstance.GetComponent<EnemyTarget>().SetObjective(objectiveTarget); 
+
+            if (lastPack && lastEnemy) 
+            {
+                enemyInstance.GetComponent<Enemy>().OnDeath += WaveComplete;
+            }
+
+            spawnLocations[Random.Range(0, spawnLocations.Count)].Spawn(enemyInstance);
+
+            currentSpawn -= 1;
+        }
+    }
+
+    private void WaveComplete(object sender, System.EventArgs e)
+    {
+        Enemy enemy = sender as Enemy;
+        enemy.OnDeath -= WaveComplete;
+
+        ResetWave();
+    }
+
+    private void ResetWave()
+    {
+        waveCount += 1;
+        currentSpawn = waveCount * spawnCount;
+        spawnTime = spawnPace;
     }
 }
